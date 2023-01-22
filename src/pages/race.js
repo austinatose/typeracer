35d9e5639729f84a95eb7f5948987ad35c729364
NeeNawAmbulance <51728263+NeeNawAmbulance@users.noreply.github.com>
2023-01-22 11:00:06 +0800

    OMG I FIXED HIDING INSTRUCTIONS

--- src/pages/race.js
+++ src/pages/race.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import useTypingGame, { CharStateType } from 'react-typing-game-hook';
+import useTypingGame from 'react-typing-game-hook';
 
 import Instructions from '../components/instructions';
 import RaceText from '../components/racetext';
@@ -16,14 +16,12 @@ const Race = () => {
   const [show, setShow] = useState(false);
 
   const {
-    states: { errorChar, correctChar, endTime, startTime, length },
-    actions: { insertTyping, resetTyping, deleteTyping },
   } = useTypingGame("The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.");
 
   return (
-    <div className="race">
-      { show ? <Instructions /> : null }
-      <RaceText onClick={() => setShow(true)} />
+    <div className="race" onClick={() => setShow(true)}>
+      { show ? null : <Instructions /> }
+      <RaceText />
     </div>
   );
 };
