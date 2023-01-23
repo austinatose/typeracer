899cde2a2049bba7c70d826391e8bf5ebd3ec245
NeeNawAmbulance <51728263+NeeNawAmbulance@users.noreply.github.com>
2023-01-23 18:19:01 +0800

    lay foundations for settings

--- src/pages/race.js
+++ src/pages/race.js
@@ -4,21 +4,30 @@ import useTypingGame from 'react-typing-game-hook';
 
 import Instructions from '../components/instructions';
 import RaceText from '../components/racetext';
+import Settings from './settings';
 
 const sentence = "The quick brown fox jumps over the lazy dog.";
 
 const Race = () => {
 
   const [showInstructions, setShowInstructions] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
 
   const {
   } = useTypingGame(sentence);
 
   return (
-    <div className="race" onClick={() => setShowInstructions(true)}>
-      { showInstructions ? null : <Instructions /> }
-      <RaceText sentence={sentence}/>
-    </div>
+    <>
+      <div className='App-header'>
+        {!showSettings ? (
+            <div className="race" onClick={() => setShowInstructions(true)}>
+              { showInstructions ? null : <Instructions /> }
+              <RaceText sentence={sentence}/>
+            </div>
+        ) : null}
+      </div>
+      {showSettings ? <Settings /> : null}
+    </>
   );
 };
 
